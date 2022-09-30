569969d39bca0dc5baa741cc2844b02d6ddd3ade
kasiadogwillo <katarzynadogwillo@gmail.com>
2022-09-30 13:03:45 +0200

    commit TypyDanychLekcja1.java file

---
+++ src/TypyDanychLekcja1.java
@@ -0,0 +1,8 @@
+public class TypyDanychLekcja1 {
+    public static void main(String[] args) {
+        //start aplikacji - wykonanie kodu - metoda main
+
+        System.out.println("This is my first program");
+            //wyświetlanie stringa na konsoli (system.out) - metoda sout
+    }
+}
