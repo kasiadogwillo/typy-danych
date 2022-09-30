d2067488acec7c16fcb3b2d8c31448a6c13eaa92
kasiadogwillo <katarzynadogwillo@gmail.com>
2022-09-30 19:54:03 +0200

    commit TypyDanychLekcja1.java file with typy danych

--- src/TypyDanychLekcja1.java
+++ src/TypyDanychLekcja1.java
@@ -2,7 +2,26 @@ public class TypyDanychLekcja1 {
     public static void main(String[] args) {
         //start aplikacji - wykonanie kodu - metoda main
 
-        System.out.println("This is my first program");
-            //wyświetlanie stringa na konsoli (system.out) - metoda sout
+        //liczby całkowite
+        System.out.println(10);
+
+        //liczby zmiennoprzecinkowe - z kropką
+        System.out.println(10.567);
+
+        //tekst - string - w cudzysłowie
+        System.out.println("Mój string");
+
+        //pojedynczy znak, np. 'a', '1' - w pojedynczym cudzysłowie
+        System.out.println('a');
+
+        //true/false
+        System.out.println(true);
+
+        //true/false
+        System.out.println(false);
+
+
+
+
     }
 }
