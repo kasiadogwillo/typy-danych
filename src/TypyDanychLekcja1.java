b9da892e6b8c8d78bfbd0f7b675b2f959ef78be2
kasiadogwillo <katarzynadogwillo@gmail.com>
2022-10-01 17:07:40 +0200

    commit TypyDanychLekcja1.java file with modulo

--- src/TypyDanychLekcja1.java
+++ src/TypyDanychLekcja1.java
@@ -1,34 +1,47 @@
 public class TypyDanychLekcja1 {
     public static void main(String[] args) {
 
-        //nazwy klas pisane są wielką literą
-        //Dane, TypyDanych itp.
+//        //4 zmienne int
+//        int a = 9;
+//        int b = 10;
+//        int c = 12;
+//        int d = 48;
+//
+//        //czy suma dwóch pierwszych liczb jest parzysta
+//        //czy suma dwóch kolejnych liczb jest parzysta
+//
+//        int sum1 = (a + b);
+//        int sum2 = (c + d);
+//
+//        //jeżeli obie sumy są parzyste - wyświetl informację, że wszystkie liczby są parzyste
+//
+//        if (sum1 % 2 == 0 && sum2 % 2 == 0)
+//            System.out.println("obie te liczby są parzyste");
+//
+//        else
+//            System.out.println("tylko jedna z tych liczb jest parzysta");
 
-        //nazwy zmiennych pisane są małą literą
-        //rodzaje zmiennej - głównie String dużą literą
-        String nazwaZmiennej = "to jest moja zmienna";
-        int number = 10;
-        char aToB = 'a';
-        boolean isFalse = false;
 
-        //formatowanie kodu
-        //ctrl + alt + L - porządkuje kod
+        //------
+        //4 zmienne int
+        int liczba1 = 123;
+        int liczba2 = 212;
+        int liczba3 = 22;
+        int liczba4 = 68;
 
-        //operator modulo - reszta z dzielenia - %
+        //czy suma dwóch pierwszych liczb jest parzysta
+        //czy suma dwóch kolejnych liczb jest parzysta
 
-        System.out.println(10 % 5); //reszta z dzielenia 0 (2*5=10)
-        System.out.println(10 % 6); //reszta z dzielenia 4
 
-        //sprawdzenie, czy liczba jest parzysta
+       int suma1 = liczba1 + liczba2;
+        System.out.println(suma1);
+       int suma2 = liczba3 + liczba4;
+        System.out.println(suma2);
 
-        if (10 % 2 == 0) {
-            System.out.println("10 lest liczbą parzystą");
-        }
-
-        if (17 % 2 == 0) {
-            System.out.println("17 jest liczbą parzystą");
-        } else {
-            System.out.println("17 nie jest liczbą parzystą");
-        }
+        //jeżeli obie sumy są parzyste - wyświetl informację, że wszystkie liczby są parzyste
+        if(suma1 % 2 == 0 && suma2 % 2 == 0)
+            System.out.println("wszystkie liczby są parzyste");
+        else
+            System.out.println("nie wszystkie liczby są parzyste");
     }
 }
