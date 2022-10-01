86512cad555543fe0563d24d77817362b1b788b1
kasiadogwillo <katarzynadogwillo@gmail.com>
2022-10-01 10:16:06 +0200

    commit TypyDanychLekcja1.java file with instrukcje sterujące

--- src/TypyDanychLekcja1.java
+++ src/TypyDanychLekcja1.java
@@ -1,48 +1,54 @@
 public class TypyDanychLekcja1 {
     public static void main(String[] args) {
 
-        //operatory matematyczne
+        //instrukcje sterujące
+        //if-else
 
-        System.out.println(2 + 2);
-        System.out.println(2 - 2);
-        System.out.println(2 * 2);
-        System.out.println(2 / 2);
-
-        System.out.println(10 / 4); //otrzymamy wynik 2 - używamy liczb całkowitych, więc wyjdzie liczba całkowita - należy użyć 10.0
-        System.out.println(10.0 / 4);
-
-        //zmiennoprzecinkowe dla liczb całkowitych
-
-        double a = 10;
-        double b = 3;
-
-        System.out.println(a / b);
-
-        //operatory porównania - czy wartość jest mniejsza, większa, równa, różna od innej
+        if (true) {
+            System.out.println("true testing"); //wartość wyświetla się w konsoli
+        }
+        if (false) {
+            System.out.println("true testing false"); //wartość nie wyświetla się w konsoli
+        }
 
         int numberOne = 2;
         int numberTwo = 4;
 
-        //w odpowiedzi boolean true/false
-        System.out.println(numberOne > numberTwo);
-        System.out.println(numberOne < numberTwo);
-        System.out.println(numberOne == numberTwo);
-        System.out.println(numberOne != numberTwo);
+        if (numberOne < numberTwo)
+            System.out.println("number One is less than number Two");
+        if (numberOne > numberTwo)
+            System.out.println("number One is more than number Two"); //fałsz - nie wyświetla się
 
-        //operatory logiczne
-        // || - lub
+        if (numberOne > numberTwo) {
+            System.out.println("One is less than Two");
+        } else { //boolean
+            System.out.println("One is more than Two"); //warunek 1 jest fałszem, więc wyświetla się warunek drugi
+        }
 
-        //jeżeli którakolwiek ze stron zwraca true, całość zwraca true
-        //jeżeli żadna ze stron nie zwraca true, całość zwraca false
+        //switch
 
-        System.out.println(numberOne > numberTwo || numberOne == 1); //false || false = false
-        System.out.println(numberOne < numberTwo || numberOne == 3); //true || false = true (pierwsza wartość to true, więc druga wartość nie jest już sprawdzana)
-        System.out.println(numberOne > numberTwo || numberOne != 1); //false || true = true (pierwsza wartość jest sprawdzana, ale druga jest true)
+        String homePage = "Home";
+        String loginPage = "Login";
+        String contactPage = "Contact";
 
-        //&& - i - obie strony muszą być true/false
+        String page = "defaultPage";
 
-        System.out.println(numberOne < numberTwo && numberOne == 2); //true && true = true
-        System.out.println(numberOne > numberTwo && numberTwo == 5); //false && false = false
-        System.out.println(numberTwo == numberOne && numberTwo == 4); //false && true = false
+        switch(page){ //przełączanie się pomiędzy wartościami
+            case "Home": //przełączamy się na stronę HOME
+                System.out.println("Switching on Home page");
+                break; //wychodzenie z przełącznika/instrukcji sterującej
+
+            case "Login":
+                System.out.println("Switching on Login page");
+                break;
+
+            case "Contact":
+                System.out.println("Switching on Contact page");
+                break;
+
+            default: //wartość dowolna, gdy nie chcemy przyjmować żadnej z wymienionych
+                System.out.println("Page not found");
+                //bez break
+        }
     }
 }
